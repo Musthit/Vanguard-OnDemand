eedb48c6d089d6180797ba3c088078b2ab296fc0
Asira <100193393+Musthit@users.noreply.github.com>
2024-06-14 19:14:55 +0200

    Disables vanguard when exit league completely.
    Now, the script will disable the SVG service on PC startup, so it will be inactive every time you boot your computer. To play, you'll need to start the game from the script to activate the services and keep it running. This way, when you exit the game, the script will handle turning off the services again."

--- Vanguard-OnDemand.bat
+++ Vanguard-OnDemand.bat
@@ -1,26 +1,57 @@
 @echo off
-echo Iniciando VGC Service...
-start "VGC Service" sc start vgc
 
-echo Iniciando VGC Tray...
-start "VGC Tray" "C:\Program Files\Riot Vanguard\vgtray.exe"
+echo Comprobando si VGC Service está habilitado...
+sc query vgc | find "ESTADO" | find "STOPPED"
+if %ERRORLEVEL% EQU 0 (
+    echo VGC Service está deshabilitado. Habilitándolo...
+    sc config vgc start=auto
+    echo Esperando 2 segundos para aplicar el cambio...
+    timeout /t 2 >NUL
+    echo Iniciando VGC Service...
+    sc start vgc
+) else (
+    echo VGC Service está habilitado. Iniciando...
+    sc start vgc
+)
+
+echo Comprobando si Vanguard Tray está deshabilitado...
+tasklist /FI "IMAGENAME eq vgtray.exe" 2>NUL | find /I "vgtray.exe" >NUL
+if %ERRORLEVEL% NEQ 0 (
+    echo Vanguard Tray no está en ejecución. Iniciando...
+    start "" "C:\Program Files\Riot Vanguard\vgtray.exe"
+) else (
+    echo Vanguard Tray ya está en ejecución.
+)
 
 echo Iniciando Riot Client...
-start "Riot Client" "D:\Games\Riot Games\League of Legends\LeagueClient.exe" --launch-product=league_of_legends --launch-patchline=live
+start "" "D:\Games\Riot Games\Riot Client\RiotClientServices.exe
 
 echo Esperando a que League of Legends inicie...
-timeout /t 30
+timeout /t 60
 
 :check
 tasklist /FI "IMAGENAME eq LeagueClient.exe" 2>NUL | find /I "LeagueClient.exe" >NUL
-if "%ERRORLEVEL%"=="0" (
+if %ERRORLEVEL% EQU 0 (
     echo League of Legends sigue en ejecución...
     timeout /t 10
     goto check
 ) else (
     echo League of Legends se ha cerrado. Deteniendo Vanguard...
-    taskkill /IM vgtray.exe /F
-    sc stop vgc
+
+   
+
     echo Vanguard se ha detenido.
+
+    echo Deshabilitando VGC Service...
+    sc stop vgc >NUL
+    sc config vgc start=disabled >NUL
+    echo VGC Service ha sido deshabilitado.
+
     pause
+
+    taskkill /F /IM vgtray.exe >NUL 2>&1
+    echo Correcto: se terminó el proceso "vgtray.exe".
+   
+
+
 )
